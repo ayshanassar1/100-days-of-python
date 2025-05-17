8136fdc0e02bc83aec95b6c250be7ebc4f490c24
Aysha Nassar <ayshanassar10000@gmail.com>
2025-05-17 13:16:06 +0530

    Basics of Python-II

--- Day 2/string methods and formatting.py
+++ Day 2/string methods and formatting.py
@@ -1,1 +1,52 @@
+# String methods
 
+text = "hello world"
+
+# Converts to uppercase
+print(text.upper())      
+#output: HELLO WORLD
+
+#Converts to lowercase
+print(text.capitalize())  
+#Output: Hello world
+
+#Replaces part of the string
+print(text.replace("world", "Python")) 
+#Output:hello Python
+
+#Splits string into list
+print(text.split())  
+#Output:['Hello,', 'Python!']
+
+#Finds the position of a word/letter
+print(text.find("world"))  
+#Output:9
+
+#Checking Start of a String
+print(text.strip().startswith("h"))  
+#Output:True
+
+#Checking End of a String
+print(text.strip().endswith("!"))    
+#Output:False
+
+#Counts how many times a character appears
+print(text.count("o"))  
+#Output:2
+
+# String formatting
+
+name = "Priya"
+age = 21
+
+#Most modern method
+print(f"My name is {name} and I'm {age} years old.")
+#Output:My name is Priya and I'm 21 years old.
+
+#format() method
+print("My name is {} and I am {} years old.".format(name, age))
+#Output:My name is Priya and I'm 21 years old.
+
+#% formatting (older style)
+print("My name is %s and I am %d years old." % (name, age))
+#Output:My name is Priya and I'm 21 years old.
